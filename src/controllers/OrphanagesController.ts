900383a7dcf93f9583557c6477a40f5ba22ba1ba
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-10-14 19:02:45 -0300

    Conclusao do upload de images.

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -23,8 +23,13 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage);
 
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
         const orphanage = orphanagesRepository.create({
-            name, latitude, longitude, about, instructions, opening_hours, open_on_weekends
+            name, latitude, longitude, about, instructions, opening_hours, open_on_weekends, images
         });
 
         await orphanagesRepository.save(orphanage);
